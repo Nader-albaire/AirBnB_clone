ef0257adf3b507b5945e0c24382e7b24016df6da
Nader-albaire <140080327+Nader-albaire@users.noreply.github.com>
2024-02-14 03:04:12 +0300

    update

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -13,7 +13,6 @@ from datetime import datetime
 from time import sleep
 from models.user import User
 
-
 class TestUser_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the User class."""
 
