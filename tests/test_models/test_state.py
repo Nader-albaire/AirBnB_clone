51f7e42a1874d9b8c6a3d5c533889050c4b2f6b1
Nader-albaire <140080327+Nader-albaire@users.noreply.github.com>
2024-02-14 03:04:53 +0300

    update

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -14,6 +14,7 @@ from datetime import datetime
 from time import sleep
 from models.state import State
 
+
 class TestState_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the State class."""
 
