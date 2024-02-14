f4a88dd978e7c0b29cda8d8201421268350e334e
Nader-albaire <140080327+Nader-albaire@users.noreply.github.com>
2024-02-14 03:03:20 +0300

    update

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -17,7 +17,6 @@ from models.state import State
 class TestState_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the State class."""
 
-
     def test_no_args_instantiates(self):
         self.assertEqual(State, type(State()))
 
